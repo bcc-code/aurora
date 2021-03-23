14d16c732acc8652e66ccffbade0961a283c8f50
Matjaz Debelak <matjaz@debelak.org>
2021-03-23 17:00:11 +0100

    Catch error in firebase function and log details

--- firebase/functions/src/handlers/firebaseToken.ts
+++ firebase/functions/src/handlers/firebaseToken.ts
@@ -40,61 +40,68 @@ passport.serializeUser((user, done) => { done(null, user) });
 passport.deserializeUser((user, done) => { done(null, user) });
 
 firebaseToken.get("/", jwtCheck, syncUserAndClaims, async (req: Request, res) => {
-  try {
-    const firebaseToken = await firebaseAdmin
-      .auth()
-      .createCustomToken(req.user[n.claims.uid], req.userClaims);
-    const userModel = new UserModel(firebaseAdmin.firestore())
-    const userRole = await userModel.getters.role(req.user[n.claims.personId]);
-    return res.send({ firebaseToken, userRole });
-  } catch (err) {
-    console.error(err);
-    return res.status(500).send({
-      message: "Something went wrong acquiring a Firebase token.",
-      error: err
-    });
-  }
+	try {
+		const firebaseToken = await firebaseAdmin
+		.auth()
+		.createCustomToken(req.user[n.claims.uid], req.userClaims);
+		const userModel = new UserModel(firebaseAdmin.firestore())
+		const userRole = await userModel.getters.role(req.user[n.claims.personId]);
+		return res.send({ firebaseToken, userRole });
+	} catch (err) {
+		console.error(err);
+		return res.status(500).send({
+			message: "Something went wrong acquiring a Firebase token.",
+			error: err
+		});
+	}
 });
 
 firebaseToken.get("/login", function(req, res, next) {
-  return passport.authenticate("auth0", { scope: "openid email profile church country", audience: config.auth0.apiAudience})(req, res, next);
+	return passport.authenticate("auth0", { scope: "openid email profile church country", audience: config.auth0.apiAudience})(req, res, next);
 });
 
 firebaseToken.get("/callback", (req: Request, res, next) => {
-  passport.authenticate("auth0", (err, user) => {
-    if (err) return next(err);
-    if (!user) return res.redirect("./login");
-    req.logIn(user, async (err) => {
-      if (err) return next(err);
-      req.user = user._json;
-      const userModel = new UserModel(firebaseAdmin.firestore())
-      const userRole = await userModel.getters.role(req.user[n.claims.personId]);
-      await syncUserAndClaims(req, res, next)
-      const firebaseToken = await firebaseAdmin.auth().createCustomToken(user.id);
-      return res.redirect((config.app.baseUrl) + "/callback?accessToken=" + user.accessToken + "&firebaseToken=" + firebaseToken + "&role=" + Buffer.from(userRole).toString('base64'));
-    });
-  })(req, res, next);
+	passport.authenticate("auth0", (err, user) => {
+		if (err) return next(err);
+		if (!user) return res.redirect("./login");
+		req.logIn(user, async (err) => {
+			if (err) return next(err);
+			req.user = user._json;
+			const userModel = new UserModel(firebaseAdmin.firestore())
+			const userRole = await userModel.getters.role(req.user[n.claims.personId]);
+			await syncUserAndClaims(req, res, next)
+			const firebaseToken = await firebaseAdmin.auth().createCustomToken(user.id);
+			return res.redirect((config.app.baseUrl) + "/callback?accessToken=" + user.accessToken + "&firebaseToken=" + firebaseToken + "&role=" + Buffer.from(userRole).toString('base64'));
+		});
+	})(req, res, next);
 });
 
 firebaseToken.post("/idtoken", async (req, res, next) => {
-  const result = await gaxios.request({
-    method: 'POST',
-    url: `https://identitytoolkit.googleapis.com/v1/accounts:signInWithCustomToken?key=${config.api.key}`,
-    data: {
-      token: req.body.token,
-      returnSecureToken: true
-    }
-  });
-  if (result.status == 200 && result.data.idToken) {
-    res.send({
-      idToken: result.data.idToken,
-      refreshToken: result.data.refreshToken,
-      expirationDate: Date.now() + parseInt(result.data.expiresIn) * 1000
+	try{
+		const result = await gaxios.request({
+			method: 'POST',
+			url: `https://identitytoolkit.googleapis.com/v1/accounts:signInWithCustomToken?key=${config.api.key}`,
+				data: {
+				token: req.body.token,
+				returnSecureToken: true
+			}
+		});
+		if (result.status == 200 && result.data.idToken) {
+			res.send({
+				idToken: result.data.idToken,
+				refreshToken: result.data.refreshToken,
+				expirationDate: Date.now() + parseInt(result.data.expiresIn) * 1000
+			});
+		}
+		res.status(500).send({
+			message: "Something went wrong acquiring an ID Token.",
+		});
+	} catch (e) {
+		console.log("idtoken throws: ", e);
+		res.status(500).send({
+			message: "Something went wrong acquiring an ID Token.",
     });
-  }
-  res.status(500).send({
-    message: "Something went wrong acquiring an ID Token.",
-  });
+	}
 });
 
 export { firebaseToken };
