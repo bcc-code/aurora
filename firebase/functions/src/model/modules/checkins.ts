726f068b2b54f1ac1caf29031d73bd4af70a064c
Matjaz Debelak <matjaz.debelak@bcc.no>
2021-05-04 09:09:55 +0200

    Don't attempt to insert undefined into firebase.

--- firebase/functions/src/model/modules/checkins.ts
+++ firebase/functions/src/model/modules/checkins.ts
@@ -93,9 +93,8 @@ export class CheckinModule extends Module {
       }
     };
 
-    this.actions.checkin = async (currentPersonId, userIds, coords) => {
-      // TODO: reject if event not open for checkin
-
+    this.actions.checkin = async (currentPersonId, userIds) => {
+      const coords =   new firestore.GeoPoint(0,0); // We keep this in case anything expects it
       const currentUser = await userModel.refs.user(currentPersonId).get();
 
       var batch = db.batch();
