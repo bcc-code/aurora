a9aef884d080d3dafb11a6a3e8a7f4723a5aa528
Matjaz Debelak <matjaz.debelak@bcc.no>
2021-05-20 21:33:57 +0200

    WIP Authenticated URL

--- firebase/functions/src/handlers/utils.ts
+++ firebase/functions/src/handlers/utils.ts
@@ -1,9 +1,22 @@
 import { Request, Response } from 'express'
 import { getPersonId } from '../model/utils'
+import * as gaxios from 'gaxios'
 
-export function getDonationURL(req: Request, res: Response) : void {
+export async function getDonationURL(req: Request, res: Response) : Promise<void> {
     const personId: string | null = getPersonId(req);
     console.log(`Donation url requested for ${personId}`);
+
+    const url = 'https://donation-api-stage.azurewebsites.net/api/Authenticate/AuthenticatedUrl'
+
+    const result = await gaxios.request({
+        method: 'POST',
+        url,
+        headers: {
+            Authorization: req.headers.authorization,
+        },
+    });
+
+    console.log(result);
 
     res.json({ url: "https://donation.bcc.no/donation" }).end();
 }
