0ba2d030cbc40120818ce6cb66cc4dab36b0201b
Matjaz Debelak <matjaz.debelak@bcc.no>
2021-07-13 23:55:29 +0200

    More debug

--- firebase/functions/src/apis/members.ts
+++ firebase/functions/src/apis/members.ts
@@ -1,8 +1,8 @@
 import fetch, { RequestInit } from 'node-fetch'
 
 enum Method {
-	POST = "POST",
-	GET = "GET",
+    POST = "POST",
+    GET = "GET",
 }
 
 interface Church {
@@ -19,65 +19,65 @@ interface Church {
 }
 
 export interface Person {
-	birthDate: string
-	firstName: string
-	lastName: string
-	displayName: string
+    birthDate: string
+    firstName: string
+    lastName: string
+    displayName: string
     churchId: number
-	guardianID: number
-	lastChangedDate: string
+    guardianID: number
+    lastChangedDate: string
     profilePicture: string | null
-	middleName: string | null
-	personID: number
-	secondGuardianID: number | null
-	related: {
-		children: Array<Person>,
-		spouse: Array<Person>,
-	},
-    church: Church,
+    middleName: string | null
+    personID: number
+    secondGuardianID: number | null
+    related: {
+        children: Array<Person>,
+        spouse: Array<Person>,
+    },
+        church: Church,
 }
 
 interface PersonListResponse {
-	total: number
-	limit: number
-	skip: number
-	data: Array<Person>
+    total: number
+    limit: number
+    skip: number
+    data: Array<Person>
 }
 
 export default class MembersAPI {
-	apiKey: string;
-	baseUrl = "https://members.bcc.no/"
+    apiKey: string;
+    baseUrl = "https://members.bcc.no/"
 
-	constructor(key: string) {
-		this.apiKey = key
-	}
+    constructor(key: string) {
+        this.apiKey = key
+    }
 
-	public async getPerson(personId: string) : Promise<Person|null> {
-		const personsList = await this.doRequest<PersonListResponse>(Method.GET, `person?personID=${personId}`)
+    public async getPerson(personId: string) : Promise<Person|null> {
+        const personsList = await this.doRequest<PersonListResponse>(Method.GET, `person?personID=${personId}`)
         if (personsList.total !== 1) {
             return null
         }
 
-         return personsList.data[0]
-	}
+        return personsList.data[0]
+    }
 
-	private async doRequest<T>(method: Method, url: string, body: Record<string, unknown> | null = null)  : Promise<T> {
-		const params : RequestInit = {
-			method,
-			headers: {
-				'Content-Type': 'application/json',
-				'x-access-token': this.apiKey,
-			},
-		}
-
-		if (method === Method.POST) {
-			params.body = JSON.stringify(body)
-		}
-
-		const res = await fetch(`${this.baseUrl}/${url}`, params)
-        if (!res.ok) {
-            console.log(res.body.read())
+    private async doRequest<T>(method: Method, url: string, body: Record<string, unknown> | null = null)  : Promise<T> {
+        const params : RequestInit = {
+            method,
+            headers: {
+                'Content-Type': 'application/json',
+                'x-access-token': this.apiKey,
+            },
         }
-		return (await res.json()) as T
-	}
+
+        if (method === Method.POST) {
+            params.body = JSON.stringify(body)
+        }
+
+        const res = await fetch(`${this.baseUrl}/${url}`, params)
+        if (!res.ok) {
+            console.log(await res.text())
+        }
+        return (await res.json()) as T
+    }
 }
