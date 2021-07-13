be0a9fc6447d4b070d46eeff6985f34aaf4daeb7
Matjaz Debelak <matjaz.debelak@bcc.no>
2021-07-13 16:48:21 +0200

    WIP - Members sync

--- firebase/functions/src/model/user.ts
+++ firebase/functions/src/model/user.ts
@@ -4,8 +4,9 @@ import { n } from './constants'
 import { IUser } from '../types/user'
 import { logger } from '../log'
 import { firestore } from 'firebase-admin'
+import { DateTime } from 'luxon'
+import MembersAPI from '../apis/members'
 import {
-    DocumentSnapshot,
     QueryDocumentSnapshot,
 } from 'firebase-functions/lib/providers/firestore'
 import { UserRecord } from 'firebase-functions/lib/providers/auth'
@@ -48,182 +49,6 @@ export class UserModel {
             return (permissionDoc.data()?.role as string) ?? 'viewer'
         }
         return 'viewer'
-    }
-
-    async updateGuardianLinkedUsersList(
-        userData: IUser,
-        update: IUser,
-        batch: firestore.WriteBatch
-    ): Promise<boolean> {
-        let modified = false
-        const users = new Map()
-        const userRefs = new Map()
-
-        const idsToAddTo: (number | null)[] = [
-            (update.guardian1Id || userData.guardian1Id) ?? null,
-            (update.guardian2Id || userData.guardian2Id) ?? null,
-        ].filter((id) => Boolean(id) && id !== userData.personId)
-
-        const idsToRemoveFrom = [
-            userData.guardian1Id && update.guardian1Id !== userData.guardian1Id
-                ? userData.guardian1Id
-                : null,
-            userData.guardian2Id && update.guardian2Id !== userData.guardian2Id
-                ? userData.guardian2Id
-                : null,
-        ].filter((id) => Boolean(id) && !idsToAddTo.includes(id))
-
-        for (const id of idsToRemoveFrom.concat(idsToAddTo)) {
-            if (id && !userRefs.has(id)) {
-                const ref = this.userRef(`${id}`)
-                userRefs.set(id, ref)
-            }
-        }
-
-        const p: Promise<void>[] = []
-        for (const [id, ref] of userRefs) {
-            if (ref instanceof firestore.DocumentReference) {
-                p.push(
-                    ref.get().then((u) => {
-                        if (u.exists) {
-                            users.set(id, u.data())
-                        }
-                        return
-                    })
-                )
-            }
-        }
-        await Promise.all(p)
-
-        for (const id2 of idsToRemoveFrom) {
-            const d = users.get(id2) as IUser
-            if (
-                d &&
-                Array.isArray(d.linkedUserIds) &&
-                d.linkedUserIds.includes(userData.personId)
-            ) {
-                d.linkedUserIds = d.linkedUserIds.filter(
-                    (val: number) => val !== userData.personId
-                )
-                batch.update(this.userRef(d.personId.toString()), {
-                    linkedUserIds: d.linkedUserIds,
-                })
-                modified = true
-            }
-        }
-        for (const id3 of idsToAddTo) {
-            const d = users.get(id3) as IUser
-            if (d) {
-                if (!d.linkedUserIds) {
-                    d.linkedUserIds = []
-                }
-                if (!d.linkedUserIds.includes(userData.personId)) {
-                    d.linkedUserIds.push(userData.personId)
-                    batch.update(this.userRef(d.personId.toString()), {
-                        linkedUserIds: d.linkedUserIds,
-                    })
-                    modified = true
-                }
-            }
-        }
-
-        return modified
-    }
-
-    async updateInternal(
-        userRef: FirebaseFirestore.DocumentReference,
-        userObj: FirebaseFirestore.DocumentSnapshot,
-        update: IUser
-    ): Promise<DocumentSnapshot> {
-        // try get existing user
-
-        const userData: IUser = userObj.data() as IUser
-        const localBatch = this.db.batch()
-        let userChanged = false
-
-        const guardiansChanged = await this.updateGuardianLinkedUsersList(
-            userData,
-            update,
-            localBatch
-        )
-
-        if (
-            update.firstName !== userData.firstName ||
-            update.lastName !== userData.lastName ||
-            (update.displayName !== null &&
-                update.displayName !== userData.displayName) ||
-            (update.uid !== null && update.uid !== userData.uid) ||
-            (update.genderId !== null &&
-                update.genderId !== userData.genderId) ||
-            (update.guardian1Id !== null &&
-                update.guardian1Id !== userData.guardian1Id) ||
-            (update.guardian2Id !== null &&
-                update.guardian2Id !== userData.guardian2Id) ||
-            (update.birthdate !== null &&
-                update.birthdate !== userData.birthdate) ||
-            (update.churchId !== null &&
-                update.churchId !== userData.churchId) ||
-            (update.churchName !== null &&
-                update.churchName !== userData.churchName) ||
-            (update.countryName !== null &&
-                update.countryName !== userData.countryName) ||
-            (update.hasMembership !== null &&
-                update.hasMembership !== userData.hasMembership)
-        ) {
-            userChanged = true
-            localBatch.update(userRef, update)
-        }
-
-        if (userChanged || guardiansChanged) {
-            log.info(`Saving changes to user ID: ${update.personId}`)
-            await localBatch.commit()
-
-            const result = await userRef.get()
-            return result
-        }
-        return userObj
-    }
-
-    async createOrUpdate(update: IUser): Promise<DocumentSnapshot> {
-        const userRef = this.userRef(`${update.personId}`)
-        const userObj = await userRef.get()
-        return userObj.exists
-            ? await this.updateInternal(userRef, userObj, update)
-            : await this.createInternal(update)
-    }
-
-    async createInternal(created: IUser): Promise<DocumentSnapshot> {
-        log.info(
-            `Creating new user, ID: ${created.personId}, displayName: ${
-                created.displayName ?? ''
-            }`
-        )
-        const localBatch = this.db.batch()
-        const linkedUserIds: number[] = []
-
-        // when creating, we should check if this user is a Guardian for any existing users
-        const g1List = this.users.where('guardian1Id', '==', created.personId)
-        const g1deps = await g1List.get()
-        g1deps.forEach((results) => {
-            if (!linkedUserIds.includes(results.data().personId)) {
-                linkedUserIds.push(results.data().personId)
-            }
-        })
-        const g2List = this.users.where('guardian2Id', '==', created.personId)
-        const g2deps = await g2List.get()
-        g2deps.forEach((results) => {
-            if (!linkedUserIds.includes(results.data().personId)) {
-                linkedUserIds.push(results.data().personId)
-            }
-        })
-        created.linkedUserIds = linkedUserIds
-        localBatch.set(this.userRef(`${created.personId}`), created)
-
-        await this.updateGuardianLinkedUsersList(created, created, localBatch)
-
-        await localBatch.commit()
-
-        return await this.userRef(`${created.personId}`).get()
     }
 
     async getUser(uid: string): Promise<UserRecord | null> {
@@ -302,6 +127,63 @@ export class UserModel {
         return customClaims
     }
 
+    async updateFromMembers(personId: string, uid: string|null = null) : Promise<IUser> {
+        console.log("Runnign update for ", personId);
+        const uRef = this.userRef(personId)
+        const data = (await uRef.get()).data() ?? {}
+
+        let needsUpdate = !data
+        needsUpdate = needsUpdate || !Boolean(data.lastUpdated)
+
+        if (data && data.lastUpdated) {
+            needsUpdate = DateTime.fromISO(data.lastUpdated) < DateTime.now().minus({week: 1})
+        }
+
+        if (!needsUpdate){
+            return data as IUser;
+        }
+
+        const mApi = new MembersAPI() //TODO: Get key from config
+        const membersData = await mApi.getPerson(personId)
+        if (!membersData) {
+            log.error("Unable to find member in members DB")
+            throw new Error("Unable to find member")
+        }
+        data.birthdate = membersData.birthDate
+
+        if (membersData.churchId) {
+            data.churchId = membersData.churchId ?? null
+        }
+        data.churchName = membersData.church.org.name
+
+        if (membersData.church.org.visitingAddress.country) {
+            data.countryName = membersData.church.org.visitingAddress.country.nameEn
+        } else {
+            console.warn("Church with no name", membersData.church.org)
+        }
+
+        const linkedUsers = membersData.related.children.map(c => c.personID)
+
+        if (uid) {
+            data.uid = uid
+        }
+
+        data.personId = membersData.personID
+        data.displayName = membersData.displayName
+        data.firstName = membersData.firstName
+        data.guardian1Id = membersData.guardianID ?? null
+        data.guardian2Id = membersData.secondGuardianID ?? null
+        data.lastName = membersData.lastName
+        data.linkedUserIds = linkedUsers
+        //data.profilePicture = membersData.profilePicture // This can only be done when we have webhooks
+        //data.profilePictureThumb = membersData.profilePicture
+        data.lastUpdated = DateTime.now().toISO()
+
+        await Promise.all(linkedUsers.map((childId: number) => this.updateFromMembers(childId.toFixed())))
+        await uRef.set(data)
+        return data as IUser
+    }
+
     async syncUserAndClaims(loggedInUser: {
         [i: string]: string
     }): Promise<{ personId?: number }> {
@@ -328,45 +210,8 @@ export class UserModel {
             throw new Error(msg)
         }
 
-        const user: IUser = {
-            personId: Number(loggedInUser[n.claims.personId]),
-            uid: loggedInUser[n.claims.uid],
-            firstName: loggedInUser[n.claims.firstName] || '',
-            lastName: loggedInUser[n.claims.lastName] || '',
-            birthdate: loggedInUser.birthdate || '',
-            displayName: `${loggedInUser[n.claims.firstName]} ${
-                loggedInUser[n.claims.lastName]
-            }`.trim(),
-        }
-
-        if (loggedInUser[n.claims.churchId]) {
-            user.churchId = Number(loggedInUser[n.claims.churchId])
-            if (user.churchId !== null) {
-                const userChurch = (
-                    await this.churchRef(`${user.churchId}`).get()
-                ).data() as { name: string; country: string }
-                if (userChurch) {
-                    user.churchName = userChurch.name
-                    user.countryName = userChurch.country
-                }
-            } else {
-                log.warn("ChurchID unusable for person ", loggedInUser[n.claims.personId], " Provided id: ", loggedInUser[n.claims.churchId])
-                user.churchName = ''
-                user.countryName = ''
-            }
-        } else {
-            log.warn("No churchID claim present for person ", loggedInUser[n.claims.personId])
-        }
-
-
-
-        user.hasMembership = loggedInUser[n.claims.hasMembership] !== undefined
-        const result = await this.createOrUpdate(user)
-        const userClaims =
-            result && result.exists
-                ? await this.updateFirebaseUser(result.data() as IUser)
-                : {}
-        return userClaims
+        const personId = (loggedInUser[n.claims.personId] as unknown as number).toFixed()
+        return await this.updateFromMembers(personId, loggedInUser[n.claims.uid])
     }
 
     async getUserDocs(
