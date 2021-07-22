e9088cd16316b0af783f951ec23cf431b22c21ba
Matjaz Debelak <matjaz.debelak@bcc.no>
2021-07-22 16:13:50 +0200

    Add list of screens to global config

--- client/src/views/GlobalSettings.vue
+++ client/src/views/GlobalSettings.vue
@@ -1,9 +1,22 @@
 <template>
     <section class="max-w-screen-xl mx-auto">
+
+<div class="grid grid-cols-1 md:grid-cols-4 gap-1 mb-5">
+    <div v-for="s in screenList" key="s">
+        <a :href="'/screens/' + s" class="flex flex row items-start rounded-lg bg-clay p-2 hover:bg-background-2-light focus:bg-background-2-light focus:outline-none focus:shadow-outline">
+            <div class="bg-bluewood text-white rounded-lg p-3 h-12 w-12 text-center">
+                <i class="fa fa-th"></i></div>
+            <div class="ml-3">
+                <p class="font-semibold">Screen {{ s }}</p>
+            </div>
+        </a>
+    </div>
+</div>
+
         <template v-if="!loaded" />
         <OneColumn v-else>
             <Form :entity="data" grid :columns="2" label-root="config" class="mb-5">
-                <Field name="canCheckin" label="Show live event in APP" type="boolean" inline />
+                <Field name="btv.canCheckin" label="Show live event in APP" type="boolean" inline />
                 <p>Note: If the user is already checked in this has no effect before app v3.6.3</p>
                 <Field name="screen.eventId" label="Event for screens" :options="filteredEvents" type="select" :selectLabel="label" selectKey="id" />
                 <Field name="screen.debug" label="Show debug info on screens" type="boolean" inline />
@@ -25,6 +38,7 @@ export default {
                 screen: {},
             },
             remoteConfig: null,
+            screenList: ['A', 'E', 'E2', 'F', 'L', 'M', 'R'],
         }
     },
     methods: {
