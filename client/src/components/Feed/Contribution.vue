fc5a6327f0108e9bb95097c49ddc6fb73b23dfaa
MasterMG2020 <markus.gaigg2020@gmail.com>
2021-10-19 13:16:43 +0200

    clean up test

--- client/src/components/Feed/Contribution.vue
+++ client/src/components/Feed/Contribution.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="rounded-xl m-2 mb-8" v-if="loaded">
+    <div class="m-2 mb-8" v-if="loaded">
         <div class="text-white leading-tight py-6 px-8 text-left rounded-t-2xl" :class="theme == 'tv' ? 'bg-primary': 'bg-slate'">
             <p class="font-semibold" :class="isBig ? 'text-4xl' : 'text-xl'">{{ displayName }}</p>
             <p class="opacity-50" :class="isBig ? 'text-3xl' : 'text-lg'">{{ churchAndCountry }}</p>
