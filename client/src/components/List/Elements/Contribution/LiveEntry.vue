235dd70b5b34a1e44fb0a3bb144fbed3b8b89b7b
MasterMG2020 <markus.gaigg2020@gmail.com>
2021-11-08 15:36:02 +0100

    added a 'Are you sure' popup

--- client/src/components/List/Elements/Contribution/LiveEntry.vue
+++ client/src/components/List/Elements/Contribution/LiveEntry.vue
@@ -2,8 +2,9 @@
     <Contribution :entry="entry" :enableLightbox="true">
         <template v-slot:default>
             <div class="flex justify-evenly text-2xl px-6 py-2">
-                <button type="button" @click.stop="rejectContribution"><i class="fas fa-trash-alt text-cerise"></i></button>
+                <button type="button" @click.stop="showRemoveConfirm = true"><i class="fas fa-trash-alt text-cerise"></i></button>
             </div>
+            <Confirm v-if="showRemoveConfirm" @cancel="showRemoveConfirm = false" @confirm="rejectContribution" :message="$t('dialogs.confirm-delete-contribution')" />
         </template>
     </Contribution>
 </template>
@@ -11,15 +12,22 @@
 <script>
 import { mapActions } from 'vuex'
 import Contribution from './Contribution.vue'
+import Confirm from '@/components/Dialogs/Confirm.vue'
 export default {
     components: {
-        Contribution
+        Contribution,
+        Confirm
     },
     props: {
         entry: {
             type: Object,
             required: true
         },
+    },
+    data: function(){
+        return {
+            showRemoveConfirm: false
+        }
     },
     methods: {
         ...mapActions('contributions', ['removeLiveRef']),
