94dcc707a313b9f1b069ab72b0f916beb08863af
Matjaz Debelak <matjaz@debelak.org>
2022-01-12 09:39:32 +0100

    Attempt to fix a JWT error

--- firebase/functions/src/middleware/passport.ts
+++ firebase/functions/src/middleware/passport.ts
@@ -18,6 +18,7 @@ const strategy = new Auth0Strategy(
         clientID: config.auth0.clientId,
         clientSecret: config.auth0.clientSecret,
         callbackURL: config.api.baseUrl + 'firebase/callback',
+        algorithms: ["RS256"],
     },
     (
         _accessToken: string,
